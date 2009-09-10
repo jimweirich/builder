196620c6d3f470db2e94771a6c91af22f1361efb
Jim Weirich <jim.weirich@gmail.com>
2009-09-10 15:26:56 -0400

    added require test unit to event testing

--- test/test_eventbuilder.rb
+++ test/test_eventbuilder.rb
@@ -1,3 +1,20 @@
+#!/usr/bin/env ruby
+
+#--
+# Portions copyright 2004 by Jim Weirich (jim@weirichhouse.org).
+# Portions copyright 2005 by Sam Ruby (rubys@intertwingly.net).
+# All rights reserved.
+
+# Permission is granted for use, copying, modification, distribution,
+# and distribution of modified versions of this work as long as the
+# above copyright notice is included.
+#++
+
+require 'test/unit'
+require 'test/preload'
+require 'builder'
+require 'builder/xmlevents'
+
 class TestEvents < Test::Unit::TestCase
 
   class Target
--- test/test_xchar.rb
+++ test/test_xchar.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
 
+#--
+# Portions copyright 2004 by Jim Weirich (jim@weirichhouse.org).
+# Portions copyright 2005 by Sam Ruby (rubys@intertwingly.net).
+# All rights reserved.
+
+# Permission is granted for use, copying, modification, distribution,
+# and distribution of modified versions of this work as long as the
+# above copyright notice is included.
+#++
+
+#!/usr/bin/env ruby
+
 require 'test/unit'
 require 'builder/xchar'
 
