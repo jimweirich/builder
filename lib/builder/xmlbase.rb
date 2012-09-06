97067cbd7c243cd71d39e327f56d9469b4145c99
Jim Weirich <jim.weirich@gmail.com>
2012-09-05 23:16:24 -0400

    Applied arity patch for to_xs
    Needed for weird compatibility issues with Rails.

--- lib/builder/xmlbase.rb
+++ lib/builder/xmlbase.rb
@@ -132,7 +132,11 @@ module Builder
       end
     else
       def _escape(text)
-        text.to_xs((@encoding != 'utf-8' or $KCODE != 'UTF8'))
+        if (text.method(:to_xs).arity == 0)
+          text.to_xs
+        else
+          text.to_xs((@encoding != 'utf-8' or $KCODE != 'UTF8'))
+        end
       end
     end
 
