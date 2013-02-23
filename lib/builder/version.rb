91544dc2c141ef2ed0d951b26b85c9d87907a77e
Jim Weirich <jim.weirich@gmail.com>
2013-02-23 06:55:03 -0800

    Bump to 3.2.0

--- lib/builder/version.rb
+++ lib/builder/version.rb
@@ -1,8 +1,8 @@
 module Builder
   VERSION_NUMBERS = [
     VERSION_MAJOR = 3,
-    VERSION_MINOR = 1,
-    VERSION_BUILD = 4,
+    VERSION_MINOR = 2,
+    VERSION_BUILD = 0,
   ]
   VERSION = VERSION_NUMBERS.join(".")
 end
